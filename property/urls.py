16e38453596013f42a2f01f4eb0a4ade105a2c37
S M Ohidul Alam <contact@ohid.info>
2018-01-02 21:44:29 +0800

    Add All Property page

--- property/urls.py
+++ property/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import CompareProperty, Property
+from .views import CompareProperty, PropertyView, PropertyListView
+from .models import Property
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="property/home.html")),
-    url(r'^add/', Property.as_view(), name="add_property"),
+    url(r'^$', PropertyListView.as_view(), name="all_properties"),
+    url(r'^add/', PropertyView.as_view(), name="add_property"),
     url(r'^compare/', CompareProperty.as_view(), name="compare_property"),
 ]
--- property/views.py
+++ property/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Property
 
-class Property(TemplateView):
+class PropertyListView(ListView):
+    model = Property
+    template_name='property/home.html'
+    def get_context_data(self, **kwargs):
+        context = dict(Property.objects.all())
+        return context
+
+class PropertyView(TemplateView):
     def get(self, request):
         return render(request, 'property/add_property.html', {})
 
