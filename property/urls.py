e3b15188cc77794ee5ff951dc3dd4446292dc90c
S M Ohidul Alam <contact@ohid.info>
2018-01-04 17:57:48 +0800

    Populate data in All Property Section

--- property/urls.py
+++ property/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import CompareProperty, AddPropertyView, PropertyListView, PropertySerializerView2
+from .views import compare_property, add_property, all_property, PropertySerializerView
 from .models import Property
 
 urlpatterns = [
-    url(r'^$', PropertyListView.as_view(), name="all_properties"),
-    url(r'^add/', AddPropertyView.as_view(), name="add_property"),
-    url(r'^compare/', CompareProperty.as_view(), name="compare_property"),
-    url(r'^rest-api/', PropertySerializerView2.as_view(), name="property_serializer")
+    url(r'^$', all_property, name="all_properties"),
+    url(r'^add/', add_property, name="add_property"),
+    url(r'^compare/', compare_property, name="compare_property"),
+    url(r'^rest-api/', PropertySerializerView.as_view(), name="property_serializer")
 ]
--- property/views.py
+++ property/views.py
@@ -1,60 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.http import HttpResponse
-from django.views.generic.list import ListView
 from .models import Property
-from .forms import AddProperty, AddPropertyType
-from rest_framework.views import APIView
+
+
 from .serializers import PropertySerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_bulk import ListCreateBulkUpdateAPIView
 
 
-class PropertyListView(ListView):
-    model = Property
-    template_name='property/home.html'
-
-    def post(self, request):
-        return HttpResponse('Works!')
-
-    def get_context_data(self, **kwargs):
-        context = super(PropertyListView, self).get_context_data(**kwargs)
-        context['property'] = Property.objects.all()
-        return context
-
-class AddPropertyView(TemplateView):
-    def get(self, request):
-        property_form = AddProperty()
-        properttype_form = AddPropertyType()
-        context = {
-          'property_form': property_form, 
-          'properttype_form': properttype_form
-        }
-        return render(request, 'property/add_property.html', context)
-    
-
-class CompareProperty(TemplateView):
-    def get(self, request):
-        return render(request, 'property/compare_property.html', {})
+from rest_framework_bulk import ListBulkCreateUpdateDestroyAPIView
 
 
-class PropertySerializerView(APIView):
-    def get(self, request):
-        all_properties = Property.objects.all()
-        serializer = PropertySerializer(all_properties, many=True)
-        return Response(serializer.data)
+def all_property(request):
+    return render(request, 'property/home.html')
 
-    def put(self, request):
-        instances = []
-        serializer = PropertySerializer(data=request.data, instance = instances, many=True)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def add_property(request):
+    return render(request, 'property/add_property.html')
 
-class PropertySerializerView2(ListCreateBulkUpdateAPIView):
+def compare_property(request):
+    return render(request, 'property/compare_property.html')
+
+
+class PropertySerializerView(ListBulkCreateUpdateDestroyAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
 
